84dcf7a9b91e7bff6e2b466267910aee80d8a270
Kwon <kwonjw5191@gmail.com>
2025-06-02 00:54:33 +0900

    test: 테스트용 커밋

--- src/main/java/com/studylog/StudylogApplication.java
+++ src/main/java/com/studylog/StudylogApplication.java
@@ -10,8 +10,6 @@ public class StudylogApplication {
 
 	public static void main(String[] args) {
 		
-		// 커밋 테스트용 수정
-		
 		// .env를 로딩해서 System 환경변수처럼 등록
 		Dotenv dotenv = Dotenv.load();
 		dotenv.entries().forEach(entry -> {

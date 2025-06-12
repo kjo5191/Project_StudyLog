1b38ee06940a3c8e17004690a682028bbe678de3
kjo5191 <kwonjw5191@gmail.com>
2025-06-12 12:10:57 +0900

    feat: Docker 배포용 .env 읽어오도록 수정

--- src/main/java/com/studylog/StudylogApplication.java
+++ src/main/java/com/studylog/StudylogApplication.java
@@ -1,5 +1,7 @@
 package com.studylog;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,15 +9,24 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class StudylogApplication {
-
 	public static void main(String[] args) {
-		
-		// .env를 로딩해서 System 환경변수처럼 등록
-		Dotenv dotenv = Dotenv.load();
+		String envDir;
+
+		// Docker 환경 (/app/.env)이 있으면 그걸 사용하고, 아니면 현재 경로
+		if (new File("/app/.env").exists()) {
+			envDir = "/app";
+		} else {
+			envDir = ".";
+		}
+
+		Dotenv dotenv = Dotenv.configure()
+			.directory(envDir)
+			.load();
+
 		dotenv.entries().forEach(entry -> {
 			System.setProperty(entry.getKey(), entry.getValue());
-		});		
+		});
+
 		SpringApplication.run(StudylogApplication.class, args);
 	}
-
 }
